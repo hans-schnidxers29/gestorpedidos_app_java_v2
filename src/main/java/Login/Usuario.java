b9e05f53f0c7c4364f176d1c167babde4aec4207
hans-schnidxers29 <hanks.patino64@gmail.com>
2025-10-24 22:53:30 -0500

    mas modificaiones en las nuevas clases

--- src/main/java/Login/Usuario.java
+++ src/main/java/Login/Usuario.java
@@ -1,4 +1,56 @@
 package Login;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="usuarios")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "nombre" , unique = true)
+    private String nombre;
+
+    @Column(name = "password", unique = true)
+    private String password;
+
+    @OneToOne
+    @JoinColumn(name = "rol_id", unique = true)
+    private Rol rol;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
 }
